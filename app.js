51772ab3e7db26cce0e30a55de39c4d6157356cb
Stacey Graham <stacey.graham1106@gmail.com>
2020-11-06 21:41:01 -0500

    defined the async function and defined the asyn function and added try and catch blocks for errors.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Create a Sequelize instance
 const Sequelize = require('sequelize'); //require the sequelize module
 
 const sequelize = new Sequelize({ //Instantiate Sequelize 
@@ -5,3 +6,14 @@ const sequelize = new Sequelize({ //Instantiate Sequelize
     storage: 'movies.db'
 }); 
    
+//Test the Connection
+
+// async IIFE
+(async () => { //Define the async function
+    try {
+        await sequelize.authenticate(); //Test the connection
+        console.log( 'Connection to the database successful!' );
+    } catch (error) {
+        console.error('Error connecting to the database: ', error);
+    }
+})();
